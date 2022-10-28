5c71ce067df9ebb369e640a7e591cb44ff98e492
Haseeb Ahmad <ahmadhaseeb049@gmail.com>
2022-10-28 10:08:37 +0500

    removed extra files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className='App'>CI/CD</div>;
+  return <div className='App'>CI CD</div>;
 }
 
 export default App;
