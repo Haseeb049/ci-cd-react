439b61d16133880ea1d3e23af41678e27be35f38
Haseeb Ahmad <ahmadhaseeb049@gmail.com>
2022-10-28 11:21:57 +0500

    text changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className='App'>CI CD</div>;
+  return <div className='App'>CI and CD</div>;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/CI CD/i);
+  const linkElement = screen.getByText(/CI and CD/i);
   expect(linkElement).toBeInTheDocument();
 });
